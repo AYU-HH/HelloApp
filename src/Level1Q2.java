d99aabbd2ac89391244643d688af99502bd59e76
AYU-HH <ak5070@srmist.edu.in>
2026-03-22 15:14:44 +0530

    Added Level 1 Q 2: Number Analysis with nested logic

---
+++ src/Level1Q2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Level1Q2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numbers = new int[5];
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+
+        for (int num : numbers) {
+            if (num > 0) {
+                if (num % 2 == 0) {
+                    System.out.println(num + " is Positive and Even");
+                } else {
+                    System.out.println(num + " is Positive and Odd");
+                }
+            } else if (num < 0) {
+                System.out.println(num + " is Negative");
+            } else {
+                System.out.println(num + " is Zero");
+            }
+        }
+        scanner.close();
+    }
+}
